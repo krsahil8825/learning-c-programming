83936b00503d5e42c416a166ca8927787c605a42
Kumar Sahil <krsahil8825@gmail.com>
2024-11-18 10:02:50 +0000

    Q09.c created and compleated

---
+++ CHAPTER_6_Printing_Pattern/Q09.c
@@ -0,0 +1,36 @@
+// write a program to print the following pattern
+
+// ASCII value is used to print the alphabets
+
+// solution:
+#include <stdio.h>
+
+// main function
+int main()
+{
+
+    // variable declaration
+    int height;
+    int Number = 1;
+    char letter = 65;
+
+    // user instruction
+    printf("This program print right trangle patterns with star\n");
+
+    // taking input from user
+    printf("Enter the Height in number:- ");
+    scanf("%d", &height);
+
+    // loop to print pattern
+    while (Number <= height)
+    {
+        for (int i = 1; i <= Number; i++)
+        {
+            printf("%c ", letter);
+            letter++;
+        }
+        printf("\n");
+        Number++;
+    }
+    return 0;
+}
