fe2b17ea30b20f31083d1121159d4fd077f8b2e6
Kumar Sahil <krsahil8825@gmail.com>
2024-12-22 14:30:17 +0000

    code correction and removed extra thing from Q1.c and  from Q2.c

--- CHAPTER_7_Pointers/Q1.c
+++ CHAPTER_7_Pointers/Q1.c
@@ -8,7 +8,8 @@ int main()
     int i = 5;
     int *j = &i;
     printf("The address of i is %p \n", &i);
-    printf("The value of j is %d\n", *j);
+    printf("The value of j is %p\n", j);
+    printf("The value at address wich is holded by j is %d\n", *j);
     printf("The value of i is %d\n", i);
     return 0;
 }
--- CHAPTER_7_Pointers/Q2.c
+++ CHAPTER_7_Pointers/Q2.c
@@ -1,7 +1,8 @@
 // Write a program having a variable ‘a’. Print the address of ‘a’. Pass this variable to
 // a function and print its address. Are these addresses same? Why?
 
-// no not same because each variable and pointer has its own address
+// yes, same because we passes the address to function it means that the value 
+// stored at the passing variable/pointer is adress of a but has its own address to
 
 #include <stdio.h>
 
@@ -10,21 +11,12 @@ void addresschecker(int *c);
 int main()
 {
     int a = 1;
-    int *b = &a; // b is a pointer which pointing to the address of a or holdes the address of a
+    // int *b = &a; // b is a pointer which pointing to the address of a or holdes the address of a
 
     printf("The value of a before calling the function of a is %d\n", a);
     printf("The address of a is %p\n", &a);
 
-    printf("The value of a before calling the function of b is %p\n", b);
-    printf("The address of a is %p\n", &b);
-    printf("The address of a is %d\n", *b);
-
-    addresschecker(b); // we can call function by pointer, by pass the address to function like this
-
-    printf("The value of a after calling the function of a is %d\n", a);
-
-    printf("calling that function again \n");
-    addresschecker(&a); // we can call function by variable, by pass the address to function like this
+    addresschecker(&a); // we can call function by pointer, by pass the address to function like this
 
     return 0;
 }
@@ -36,7 +28,7 @@ void addresschecker(int *c)
 
     printf("The value of c is %p\n", c); // here %p is used not %d because it pointer variable it stores adress
 
-    printf("The value stored in *c is %d\n", *c); // in this line says that what value is stord at this address that is stored/pointed by *c
+    printf("The value at address which is stored by *c is %d\n", *c); // in this line says that what value is stord at this address that is stored/pointed by *c
 
     *c = 5; // this will change the value of a because this function is call by refrence
 }
